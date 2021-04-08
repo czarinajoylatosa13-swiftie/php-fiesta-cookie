7ef31a974222dd64d19648d6619476f9f2e1789a
czarinajoylatosa13-swiftie <2015105913N@psu.palawan.edu.ph>
2021-04-08 09:45:38 +0700

    01sa.php done

---
+++ 01sa.php
@@ -0,0 +1,27 @@
+<?php
+	$swiftie = array(
+		array(1,'Bulbasaur','Grass','Poison','Overgrow',45,49,49),
+		array(2,'Ivysaur','Grass','Poison','Overgrow',60,62,63),
+		array(3,'Venusaur','Grass','Poison','Overgrow',80,82,83),
+		array(4,'Charmander','Fire','N/A','Blaze',39,52,43),
+		array(5,'Charmeleon','Fire','N/A','Blaze',58,64,58),
+		array(6,'Charizard','Fire','Flying','Blaze',78,84,78),
+		array(7,'Squirtle','Water','N/A','Torrent',44,48,65),
+		array(8,'Wartortle','Water','N/A','Torrent',59,63,80),
+		array(9,'Blastoise','Water','N/A','Torrent',79,83,100),
+		array(10,'Caterpie','Bug','N/A','Shield Dust',50,20,25),
+	);
+
+	print_r ($swiftie);
+	echo "<br><br>";
+	echo count ($swiftie);
+	echo "<br><br>";
+	
+	$favorite = $swiftie[5][1];
+	
+	foreach($swiftie as $swiftie){
+		echo $swiftie[1];
+		echo '<br>';
+	}
+
+?>
